3beb05ae6120e7972139653e0b48fe10580019c5
Mou Yuan Yap <mouyuanyap@yahoo.com>
2021-02-03 19:26:28 +0800

    Update build.py

--- maskrcnn_benchmark/data/transforms/build.py
+++ maskrcnn_benchmark/data/transforms/build.py
@@ -37,8 +37,8 @@ def build_transforms(cfg, is_train=True):
         [
             color_jitter,
             T.Resize(min_size, max_size),
-            T.RandomCrop((500,500)),
-            T.RandomRotateAug(True),
+            #T.RandomCrop((500,500)),
+            #T.RandomRotateAug(True),
             T.RandomHorizontalFlip(flip_horizontal_prob),
             T.RandomVerticalFlip(flip_vertical_prob),
             T.ToTensor(),
